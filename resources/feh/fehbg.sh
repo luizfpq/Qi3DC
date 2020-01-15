0701f1d522348ddb40fe9e0a1ee81236a02eda54
LuizQuirino <luizfpq@gmail.com>
2020-01-15 10:59:50 -0300

    create base dir structure

---
+++ resources/feh/fehbg.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#exec --no-startup-id feh --bg-fill /home/$USER/Imagens/Wallpaper/main.jpg
+feh --bg-fill /home/$USER/Imagens/Wallpaper/main.jpg
+
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# 
+# ScriptName        : setup.sh
+# Description       : 
+# Args              :
+# Author            : LuizQuirino
+# Email             : luizfpq@gmail.com
+# Since             :
+# Version           :
