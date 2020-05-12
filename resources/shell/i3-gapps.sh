911de0b8362a109b73723cb9485b900423d714b4
LuizQuirino <luizfpq@gmail.com>
2020-05-12 02:47:52 -0300

    add i3-gapps dependencies

--- resources/shell/i3-gapps.sh
+++ resources/shell/i3-gapps.sh
@@ -11,15 +11,17 @@
 
 i3-gapps() {
 
-  sudo apt install libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
-
+  sudo apt install libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev build-essential python python-pip compton feh i3status i3lock suckless-tools neofetch
+  pip install setuptools
+  rm -rf i3-gapps
   git clone https://www.github.com/Airblader/i3 i3-gaps
   cd i3-gaps
   git checkout gaps && git pull
   autoreconf --force --install
   rm -rf build
   mkdir build
-  cd build ../configure --prefix=/usr --sysconfdir=/etc
+  cd build 
+  ../configure --prefix=/usr --sysconfdir=/etc
   make
   sudo make install
   cd .. && rm -rf i3-gapps
--- resources/shell/powerline.sh
+++ resources/shell/powerline.sh
@@ -10,6 +10,7 @@
 
 installPowerline() {
   cd /tmp &&
+  rm -rf powerline-shell &&
   git clone https://github.com/b-ryan/powerline-shell &&
   cd powerline-shell &&
   sudo python setup.py install
