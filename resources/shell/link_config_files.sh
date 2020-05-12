d0d268e7373b0c623628c1147c3c1dad46d04186
LuizQuirino <luizfpq@gmail.com>
2020-05-12 03:37:44 -0300

    fix link files

--- resources/shell/link_config_files.sh
+++ resources/shell/link_config_files.sh
@@ -9,15 +9,7 @@
 # Version           : 0.0.1 2020-01-15
 link_config_files() {
 
-    #i3 base folder
-    #rm -rf ~/.config/i3/
-    cp -Rfv ./resources/i3 ~/.config/i3
-
-    #neofetch
-    #rm -rf ~/.config/neofetch/
-    cp -Rfv ./resources/neofetch/ ~/.config/neofetch
-
-    #compton
-    #ln -s ~/.Qi3DC/compton.conf ~/.config/compton.conf
+    #config base folder
+    cp -Rfv ./resources/.config ~/
 
 }
